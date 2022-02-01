f34ee79aed94ab717e9f6da139b4ffcde91486e9
jyoti0552cs171011 <jyotikumarisilauta@gmail.com>
2022-02-01 19:54:14 +0530

    initial Setup

---
+++ practice.js
@@ -0,0 +1,26 @@
+
+<html>
+<body>
+
+<input type="text"/>
+<input type="button" id="Increasing" value="Increasing"/> <br/><br/>
+
+<input type="button" id="Decreasing" value="Decreasing"/>
+
+<script>
+	$(document).ready(function(){
+    	$("#Increasing").click(function(){
+        	if($('#))
+        
+        })
+    
+    
+    
+    })
+
+
+
+</script>
+
+</body>
+</html>
